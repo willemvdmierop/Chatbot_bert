f47f77309cab9fd5b448e62a518ca47260ef7836
willemvdmierop <57663191+willemvdmierop@users.noreply.github.com>
2020-03-11 14:34:51 +0000

    Cornell data + implementation training loop
    First part of finetune training loop

--- FineTune.py
+++ FineTune.py
@@ -11,6 +11,7 @@ import torchvision
 # pytorch layers
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 from torch.nn import Sequential, Embedding, Linear, Conv1d, MaxPool1d, LSTM
 from torch.nn import CrossEntropyLoss as CrossEntropyLoss
 import torch.utils.data as data
@@ -18,7 +19,7 @@ from torch.jit import script, trace
 from torch.utils.data import DataLoader as DataLoader
 from collections import Counter, OrderedDict
 from transformers import BertTokenizer
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoModel, BertForPreTraining
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoModel, BertForMaskedLM
 from transformers import AutoModelWithLMHead, AutoTokenizer
 from transformers import AdamW
 # load the dataset interface
@@ -38,12 +39,12 @@ device = 'cpu'
 if (torch.cuda.is_available()):
     device = torch.device('cuda')
 
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-model = BertForPreTraining.from_pretrained('bert-base-uncased')
+
+
 
 # Todo this implementation is for scibert
 tokenizer_scibert = AutoTokenizer.from_pretrained("/Users/willemvandemierop/Google Drive/DL Prediction (706)/scibert_scivocab_uncased")
-model_scibert = BertForPreTraining.from_pretrained("/Users/willemvandemierop/Google Drive/DL Prediction (706)/scibert_scivocab_uncased")
+model_scibert = BertForMaskedLM.from_pretrained("/Users/willemvandemierop/Google Drive/DL Prediction (706)/scibert_scivocab_uncased")
 #print('scibert model', model_scibert)
 # return the list of OrderedDicts:
 # a total of 83097 dialogues
@@ -82,10 +83,26 @@ def load_data(**train_pars):
 load_data_pars = {'stage': 'train', 'num_workers': 3}
 dataset = load_data(**load_data_pars) #this returns a dataloader
 print('\n' + 40 * '#',"Now FineTuning", 40 * '#')
-
 # Load the BERT tokenizer.
 print('Loading BERT tokenizer...')
+
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+
+params = list(model.named_parameters())
+
+print('The BERT model has {:} different named parameters.\n'.format(len(params)))
+
+print('==== Embedding Layer ====\n')
+for p in params[0:5]:
+    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+print('\n==== First Transformer ====\n')
+for p in params[5:21]:
+    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+print('\n==== Output Layer ====\n')
+for p in params[-4:]:
+    print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
+
 
 device = 'cpu'
 if device == 'cuda':
@@ -99,17 +116,36 @@ optim_pars = {'lr': lrate, 'weight_decay': 1e-3}
 optimizer = AdamW(model.parameters(), **optim_pars)
 
 current_batch = 0
+total_phrase_pairs = 0
 epochs = 1
+loss_list = []
 for epoch in range(epochs):
-    for idx, val in enumerate(dataset):
-        data = val[0][1]
-        print("gittest")
-        tokenized = tokenizer.tokenize((val))
+    t0 = time.time()
+    print('======== Training =======')
+    print('======== Epoch {:} / {:} ========'.format(epoch + 1, epochs))
+    total_loss = 0
+    for idx, X in enumerate(dataset):
+        model.zero_grad()
+        # number of phrases
+        # X[0] is just the index
+        # X[1] is the dialogue, X[1][0] are input phrases, X[1][1] are output phrases
+        batch_size = X[1][0].size()[1]
+        # number of tokens in a sequence 
+        seq_length = X[1][0].size()[2]
+        total_phrase_pairs += batch_size
 
+        input_data = X[1][0]
+        input_data = input_data.view(-1, batch_size, seq_length)[0]
+        outputs = model(inputs_ids)
 
+        loss = outputs[1]
+        total_loss += loss
+        loss.backward()
+        optimizer.step()
         optimizer.zero_grad()
+    average_loss = total_loss/len(dataset)
+    loss_list.append(average_loss)
+    print("average loss '{}' and train time '{}'".format(average_loss,(time.time()-t0)))
 
-        break
-    break
 
 
