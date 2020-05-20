ce9c587a620374261d7f7b3db3d78939e2159bc0
willemvdmierop <57663191+willemvdmierop@users.noreply.github.com>
2020-05-20 13:05:49 +0200

    cosine hard restart schedule

--- Plot_graphs_with_pickle_loading.py
+++ Plot_graphs_with_pickle_loading.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 metrics1 = torch.load("Metrics/metrics_65epochs_gradientclipping_WD001_lr0001.pkl")
-metrics2 = torch.load("Metrics/metrics_lr001_wd001")
-metrics3 = torch.load("Metrics/metrics_par_opt_lr00001")
+metrics2 = torch.load("Metrics/metrics_hard_reset_not60ep")
+metrics3 = torch.load("Metrics/metrics_WD_001_lr0001_Sched_True_60_epochs.pkl")
 #metrics4 = torch.load("Metrics/metrics_WD00001_lr001_grad_clip_nosched.pkl")
 stop = 100
 single_plots = False
@@ -25,17 +25,17 @@ Q3_metrics3 = metrics3['q_metrics'][2]
 #Q2_metrics4 = metrics4['q_metrics'][1]
 #Q3_metrics4 = metrics4['q_metrics'][2]
 
-df_q11 = pd.DataFrame(Q1_metrics1).loc[:2519]
-df_q21 = pd.DataFrame(Q2_metrics1).loc[:2519]
-df_q31 = pd.DataFrame(Q3_metrics1).loc[:2519]
+df_q11 = pd.DataFrame(Q1_metrics1).loc[:1805]
+df_q21 = pd.DataFrame(Q2_metrics1).loc[:1805]
+df_q31 = pd.DataFrame(Q3_metrics1).loc[:1805]
 
-df_q12 = pd.DataFrame(Q1_metrics2).loc[:2519]
-df_q22 = pd.DataFrame(Q2_metrics2).loc[:2519]
-df_q32 = pd.DataFrame(Q3_metrics2).loc[:2519]
+df_q12 = pd.DataFrame(Q1_metrics2).loc[:1805]
+df_q22 = pd.DataFrame(Q2_metrics2).loc[:1805]
+df_q32 = pd.DataFrame(Q3_metrics2).loc[:1805]
 
-df_q13 = pd.DataFrame(Q1_metrics3).loc[:2519]
-df_q23 = pd.DataFrame(Q2_metrics3).loc[:2519]
-df_q33 = pd.DataFrame(Q3_metrics3).loc[:2519]
+df_q13 = pd.DataFrame(Q1_metrics3).loc[:1805]
+df_q23 = pd.DataFrame(Q2_metrics3).loc[:1805]
+df_q33 = pd.DataFrame(Q3_metrics3).loc[:1805]
 
 #df_q14 = pd.DataFrame(Q1_metrics4)
 #df_q24 = pd.DataFrame(Q2_metrics4)
@@ -43,16 +43,17 @@ df_q33 = pd.DataFrame(Q3_metrics3).loc[:2519]
 #df_q1.to_csv("Q1-test.csv")
 #print(df_q1[0])
 window = 20
-x = np.linspace(start = 0, stop = 60, num= 2520)
+stop = 43
+x = np.linspace(start = 0, stop = stop, num= stop*42)
 print(x)
 print(x.shape)
 
 
 fig, ax = plt.subplots(3,1,figsize = (12,10))
 
-ax[0].plot(x,df_q12[0].rolling(window).mean(), color = 'yellow', label = 'BLEU score LR 0.001')
-ax[0].plot(x,df_q11[0].rolling(window).mean(), color = 'red',label = 'BLEU score LR 0.0001')
-ax[0].plot(x,df_q13[0].rolling(window).mean(), color = 'blue', label = 'BLEU score LR 0.00001')
+ax[0].plot(x,df_q11[0].rolling(window).mean(), color = 'yellow', label = 'BLEU score LR 0.0001')
+ax[0].plot(x,df_q12[0].rolling(window).mean(), color = 'red',label = 'BLEU score Hard reset')
+ax[0].plot(x,df_q13[0].rolling(window).mean(), color = 'blue', label = 'BLEU score cosine warmup')
 #ax[0].plot(x,df_q14[0].rolling(window).mean(), color = 'cyan', label = 'BLEU score WD 0.00001')
 #ax[0].plot(x,df_q1[1].rolling(window).mean(), label = 'Precision')
 #ax[0].plot(x,df_q1[2].rolling(window).mean(), label = 'Recal')
@@ -62,9 +63,9 @@ ax[0].set_xlabel("count")
 ax[0].set_ylabel("Score")
 ax[0].legend(bbox_to_anchor=(1, 1.03))
 
-ax[1].plot(x,df_q22[0].rolling(window).mean(), color = 'yellow', label = 'BLEU score LR 0.001')
-ax[1].plot(x,df_q21[0].rolling(window).mean(), color = 'red', label = 'BLEU score LR 0.0001')
-ax[1].plot(x,df_q23[0].rolling(window).mean(), color = 'blue', label = 'BLEU score LR 0.00001')
+ax[1].plot(x,df_q21[0].rolling(window).mean(), color = 'yellow', label = 'BLEU score LR 0.001')
+ax[1].plot(x,df_q22[0].rolling(window).mean(), color = 'red', label = 'BLEU score Hard reset')
+ax[1].plot(x,df_q23[0].rolling(window).mean(), color = 'blue', label = 'BLEU score cosine warmup')
 #ax[1].plot(x,df_q24[0].rolling(window).mean(), color = 'cyan', label = 'BLEU score WD 0.00001')
 #ax[1].plot(x,df_q2[1].rolling(window).mean(), label = 'Precision')
 #ax[1].plot(x,df_q2[2].rolling(window).mean(), label = 'Recal')
@@ -74,9 +75,9 @@ ax[1].set_xlabel("count")
 ax[1].set_ylabel("Score")
 ax[1].legend(bbox_to_anchor=(1, 1.03))
 
-ax[2].plot(x,df_q32[0].rolling(window).mean(), color = 'yellow', label = 'BLEU score LR 0.001')
-ax[2].plot(x,df_q31[0].rolling(window).mean(), color = 'red', label = 'BLEU score LR 0.0001')
-ax[2].plot(x,df_q33[0].rolling(window).mean(), color = 'blue', label = 'BLEU score LR 0.00001')
+ax[2].plot(x,df_q31[0].rolling(window).mean(), color = 'yellow', label = 'BLEU score LR 0.001')
+ax[2].plot(x,df_q32[0].rolling(window).mean(), color = 'red', label = 'BLEU score Hard reset')
+ax[2].plot(x,df_q33[0].rolling(window).mean(), color = 'blue', label = 'BLEU score cosine warmup')
 #ax[2].plot(x,df_q34[0].rolling(window).mean(), color = 'cyan', label = 'BLEU score WD 0.00001')
 #ax[2].plot(x,df_q2[1].rolling(window).mean(), label = 'Precision')
 #ax[2].plot(x,df_q2[2].rolling(window).mean(), label = 'Recal')
